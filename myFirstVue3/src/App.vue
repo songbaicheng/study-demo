70fd390b7cdf747f8b293e8328175647626ab672
songbaicheng <2524218694@qq.com>
2023-07-12 23:42:54 +0800

    modify demo

--- myFirstVue3/src/App.vue
+++ myFirstVue3/src/App.vue
@@ -1,12 +1,17 @@
-<script setup lang="ts">
-let count:number = 0
-
-</script>
-
 <template>
   <div>
-    <button @click="count++">{{ count }}</button>
+    <button @click="increment">{{ count }}</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
 
 <style></style>
