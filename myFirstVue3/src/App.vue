03ea2c84ccd499d778e87e52759c515a5b8de820
songbaicheng <2524218694@qq.com>
2023-07-20 17:02:25 +0800

    add vue component code

--- myFirstVue3/src/App.vue
+++ myFirstVue3/src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import CodingWriteStyle from "./views/CodingWriteStyle.vue";
+import VueComponent from "./views/VueComponent.vue";
 </script>
 
 <template>
+  <div>
+    三种代码格式
+  </div>
   <CodingWriteStyle />
+  <div>
+    Vue 默认指令组件
+  </div>
+  <VueComponent />
 </template>
---
+++ myFirstVue3/src/views/VueComponent.vue
@@ -0,0 +1,7 @@
+<script setup>
+import VueText from '../components/component/VueText.vue'
+</script>
+
+<template>
+    <VueText />
+</template>
