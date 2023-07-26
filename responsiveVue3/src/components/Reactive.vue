6635f84c6e565ba0d6b12c1584b69d1dd96e0fa4
songbaicheng <2524218694@qq.com>
2023-07-26 16:52:05 +0800

    add responsive code

--- responsiveVue3/src/components/Reactive.vue
+++ responsiveVue3/src/components/Reactive.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+let form = reactive({
+    name: 'songbaicheng',
+    age: 23
+})
+
+let submit = () => {
+    console.log(form)
+}
+
+</script>
+
 <template>
-    reactive
+<form>
+    <input type="text" v-model="form.name">
+    <br>
+    <input type="text" v-model="form.age">
+    <br>
+    <button @click.prevent="submit">提交</button>
+</form>
 </template>
--- responsiveVue3/src/components/Ref.vue
+++ responsiveVue3/src/components/Ref.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+import { ref, shallowRef, triggerRef } from 'vue'
 
+// 非响应式对象
+let people = { name: 'songbaicheng' }
+let change = () => {
+    people.name = 'baicheng'
+    console.log(people.name)
+}
+
+// 深层响应式对象，可以循环影响到最底层
+let animal = ref({ name: 'bird' })
+let update = () => {
+    animal.value.name = 'cat'
+    console.log(animal.value.name)
+}
+
+// 浅层响应式对象，只能影响到 .value
+let animal1 = shallowRef({ name: 'dog' })
+let update1 = () => {
+    animal1.value = {
+        name: 'fish'
+    }
+    console.log(animal1.value.name)
+}
+</script>
 
 <template>
-    ref
+非响应式：{{ people.name }} <button @click="change">点我更换</button>
+<br>
+深层响应式：{{ animal.name }} <button @click="update">点我更换</button>
+<br>
+浅层响应式：{{ animal1.name }} <button @click="update1">点我更换</button>
 </template>
