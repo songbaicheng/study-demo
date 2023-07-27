ae0584d68491f2ee939fd3e8c76cb8dec1ded9ea
songbaicheng <2524218694@qq.com>
2023-07-27 11:15:16 +0800

    add toRef code

---
+++ responsiveVue3/src/components/To.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, toRef, toRefs } from 'vue'
+
+let people = ref({
+    name: 'songbaicheng',
+    age: 23,
+    tel: 123456
+})
+
+let peopleTel = toRef(people) // 只对响应式对象做修改
+
+let change = () => {
+    peopleTel.value.tel = 8888
+    console.log(peopleTel)
+}
+</script>
+
+<template>
+{{ people }}
+
+<button @click="change">改变</button>
+</template>
--- responsiveVue3/src/views/ResponsiveBase.vue
+++ responsiveVue3/src/views/ResponsiveBase.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Reactive from '../components/Reactive.vue'
 import Ref from '../components/Ref.vue'
+import To from '../components/To.vue'
 </script>
 
 <template>
     <Reactive />
     <br/>
     <Ref />
+    <br/>
+    <To />
 </template>
